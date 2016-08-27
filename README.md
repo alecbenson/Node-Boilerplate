ea827d8ba5a7d8f29f730cfb3bd7faf26a518334
alec <abenson@wpi.edu>
2016-08-27 12:16:17 -0400

    node js skeleton

---
+++ README.md
@@ -0,0 +1,3 @@
+# Node-Boilerplate
+Basic boilerplate for a node and angular webapp
+Do search and replace for `node-boilerplate` and `nodeboilerplate`
---
+++ public/js/controllers/navbar-controller.js
@@ -0,0 +1,5 @@
+'use strict';
+angular.module('nodeboilerplate')
+	.controller('navbarController', function ($scope) {
+		$scope.title = 'nodeboilerplate';
+	});
